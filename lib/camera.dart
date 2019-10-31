f1c9e97cbe6bee64d4266e67257a22a8df5d91bf
Matias Jensen <sejemus@yahoo.dk>
2019-10-31 13:54:52 +0100

    Write to image

--- lib/camera.dart
+++ lib/camera.dart
@@ -1,29 +1,49 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:image/image.dart' as RealImage;
 
 class Camera extends StatefulWidget {
-  Camera({Key key}) : super(key: key);
-
   @override
-  CameraState createState() => CameraState();
+  _CameraState createState() => _CameraState();
 }
 
-class CameraState extends State<Camera> {
+class _CameraState extends State<Camera> {
+  File _image;
+
+  Future getImage() async {
+    File image = await ImagePicker.pickImage(source: ImageSource.camera);
+    RealImage.Image test = RealImage.decodeImage(image.readAsBytesSync());
+
+    for(int i = 0; i < 100; i++) {
+      for(int j = 0; j < 100; j++) {
+        test.setPixelRgba(i, j, 255, 0, 0);
+      }
+    }
+
+    image.writeAsBytesSync(RealImage.encodePng(test));
+
+    setState(() {
+      _image = image;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
       appBar: AppBar(
-        title: Text('Camera'),
+        title: Text('Image Picker Example'),
       ),
-
-      body: Container(
-        child: Center(
-          child: Text(
-            'Camera',
-            style: TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold),
-          ),
-        ),
+      body: Center(
+        child: _image == null
+            ? Text('No image selected.')
+            : Image.file(_image),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: getImage,
+        tooltip: 'Pick Image',
+        child: Icon(Icons.add_a_photo),
       ),
     );
   }
